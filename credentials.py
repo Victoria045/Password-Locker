0a160cd72088ede8671a1590521a916de6452ac8
Victoria045 <victoriaberyl45@gmail.com>
2021-10-22 15:58:01 +0300

    method that defines user credentials to be stored

--- credentials.py
+++ credentials.py
@@ -3,4 +3,10 @@ class User:
     Credentials class that generates new instances of credentials
     """
 
-    user_list = [] #Empty credentials list
+    credentials_list = [] #Empty credentials list
+
+
+    def __init__(self, account_name, username, password):
+        self.account_name = account_name
+        self.username = username
+        self.password = password

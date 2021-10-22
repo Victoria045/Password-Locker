ae0c2ff1ff341877883a4948faac7657aebd1792
Victoria045 <victoriaberyl45@gmail.com>
2021-10-22 14:04:31 +0300

    initialize the user class

--- user.py
+++ user.py
@@ -4,3 +4,8 @@ class User:
     """
 
     user_list = [] #Empty user list
+
+    def __init__(self, account_name, username, password):
+        self.account_name = account_name
+        self.username = username
+        self.password = password

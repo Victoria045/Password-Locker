ef27db46bcb3897a3ecb482f5a0ab864d44b5b5b
Victoria045 <victoriaberyl45@gmail.com>
2021-10-22 14:21:28 +0300

    create test class that defines test cases

---
+++ user_test.py
@@ -0,0 +1,17 @@
+import unittest # Importing the unittest module
+from user import User # Importing the user class
+
+class TestContact(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()

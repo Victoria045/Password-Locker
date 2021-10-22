fd5c1485bb22823c428a7bb00ea7ce829b7ec5d2
Victoria045 <victoriaberyl45@gmail.com>
2021-10-22 14:06:06 +0300

    save user objects into user list

--- user.py
+++ user.py
@@ -5,7 +5,14 @@ class User:
 
     user_list = [] #Empty user list
 
+
     def __init__(self, account_name, username, password):
         self.account_name = account_name
         self.username = username
         self.password = password
+
+    def save_user(self):
+        """
+        save_user method saves user objects into user_list
+        """
+        User.user_list.append(self)

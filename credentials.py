ff71b1c7028be1a608713fc9b260a3dc7a04fbf6
Victoria045 <victoriaberyl45@gmail.com>
2021-10-23 12:35:03 +0300

    import random module to gnerate random passwords

--- credentials.py
+++ credentials.py
@@ -1,18 +1,1 @@
-class Credentials:
-    """
-    Credentials class that generates new instances of credentials
-    """
-
-    credentials_list = [] #Empty credentials list
-
-
-    def __init__(self, account_name, username, password):
-        self.account_name = account_name
-        self.username = username
-        self.password = password
-
-    def save_details(self):
-        """
-        saves a new credential to the credentials list
-        """
-        Credentials.credentials_list.append(self)
+import random

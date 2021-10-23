f08a4e4e268e0849194bdbc6304574d1495456f3
Victoria045 <victoriaberyl45@gmail.com>
2021-10-23 13:28:09 +0300

    create method to test if the credential object is saved into the credentials list

--- credentials_test.py
+++ credentials_test.py
@@ -19,3 +19,12 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credential.account,'Twitter')
         self.assertEqual(self.new_credential.userName,'Victoria_Beryl')
         self.assertEqual(self.new_credential.password,'1@xy%zw!')
+
+# second test <-- calls save_contact method to the newly generated object-->
+    def test_save_credential(self):
+        """
+        test case to test if the credential object is saved into the credentials list
+
+        """
+        self.new_credential.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list),1)

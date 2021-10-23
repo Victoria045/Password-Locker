0866fd40f8a9aaddef7e6381b2784e13d9dc2783
Victoria045 <victoriaberyl45@gmail.com>
2021-10-23 13:21:24 +0300

    create method that runs before each individual credentials test methods run

--- credentials_test.py
+++ credentials_test.py
@@ -3,3 +3,10 @@ class TestCredentials(unittest.TestCase):
     A test class that defines test cases for credentials class
 
     """ 
+
+    def setUp(self):
+        """
+        Method that runs before each individual credentials test methods run
+
+        """
+        self.new_credential = Credentials('Twitter','Victoria_Bryl','1@xy%zw!')

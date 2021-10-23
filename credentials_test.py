e931d50ced932de84ca538f983aa5189e673a1a8
Victoria045 <victoriaberyl45@gmail.com>
2021-10-23 13:23:30 +0300

    method to test if our objects are instantiated correctly

--- credentials_test.py
+++ credentials_test.py
@@ -4,9 +4,18 @@ class TestCredentials(unittest.TestCase):
 
     """ 
 
+# First test <-- Testing if our objects are instantiated correctly-->
     def setUp(self):
         """
         Method that runs before each individual credentials test methods run
 
         """
-        self.new_credential = Credentials('Twitter','Victoria_Bryl','1@xy%zw!')
+        self.new_credential = Credentials('Twitter','Victoria_Beryl','1@xy%zw!')
+
+    def test_init(self):
+        """
+        Test case to check if a new Credentials instance has been initialized correctly
+        """
+        self.assertEqual(self.new_credential.account,'Twitter')
+        self.assertEqual(self.new_credential.userName,'Victoria_Beryl')
+        self.assertEqual(self.new_credential.password,'1@xy%zw!')

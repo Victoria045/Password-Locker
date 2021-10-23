6946d9e5225ca9110715eacff3270f4debb6c907
Victoria045 <victoriaberyl45@gmail.com>
2021-10-23 12:51:46 +0300

    method to verify user exists

--- credentials.py
+++ credentials.py
@@ -1,8 +1,21 @@
 import random
 import pyperclip
+import user from User
 
-class Credentials():
+class Credentials:
     """
     Class that generates new instances of credentials
     """
     credentials_list = []
+
+
+    @classmethod
+    def verify_user(cls,username, password):
+        """
+        method to verify whether the user is in our user_list or not
+        """
+        existing_user = ""
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                    existing_user == user.username
+        return existing_user

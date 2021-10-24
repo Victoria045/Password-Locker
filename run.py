cf3627471397a277be192630b66bd58925c8d1e5
Victoria045 <victoriaberyl45@gmail.com>
2021-10-24 09:53:36 +0300

    create a new user with username and password

--- run.py
+++ run.py
@@ -12,3 +12,11 @@ def pattern():
 pattern()
 print ('\n')
 print("*"*100)
+
+
+def create_new_user(username,password):
+    '''
+    Function to create a new user with a username and password
+    '''
+    new_user = User(username,password)
+    return new_user

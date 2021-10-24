a58efe82513fac32213778e5904ec45ff55161e6
Victoria045 <victoriaberyl45@gmail.com>
2021-10-24 10:02:47 +0300

    create function to delete saved credentials

--- run.py
+++ run.py
@@ -59,3 +59,10 @@ def display_accounts_details():
     Function that returns all the saved credential.
     """
     return Credentials.display_credentials()
+
+def delete_credential(credentials):
+    """
+    Function to delete a Credentials from credentials list
+
+    """
+    credentials.delete_credentials()

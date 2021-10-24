80a53af7f0d3ecfa97429d08f7963fbe056971ac
Victoria045 <victoriaberyl45@gmail.com>
2021-10-24 07:23:59 +0300

    create the pass lock pattern

--- run.py
+++ run.py
@@ -1,2 +1,14 @@
 from user import User 
 from credentials import Credentials
+
+print("*"*100)
+def pattern():
+	print("               ______                        __                       __   ___              ")
+	print("              |   _  \                      |  |                     |  | /  /              ")
+	print("              |  |_)  ) ____  ___   ___     |  |      _____    _____ |  |/__/               ")
+	print("              |   _ _/ / _  |/ __  / __     |  |     /  _  \  /  __  |  |\  \               ")
+	print("              |  |    / (_| |\__ \ \__ \    |  |___ (  (_)  )(  (__  |  | \  \              ")
+	print("              |__|    \_____| ___/  ___/    |______) \_____/  \_____ |__|  \__\             ")
+pattern()
+print ('\n')
+print("*"*100)

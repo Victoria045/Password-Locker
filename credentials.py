d28ab631fa1860f5900e1d88b3069c62d64a5138
Victoria045 <victoriaberyl45@gmail.com>
2021-10-26 13:53:00 +0300

    modify credentials class

--- credentials.py
+++ credentials.py
@@ -17,17 +17,6 @@ class Credentials:
         self.user_name = user_name
         self.password = password
 
-    @classmethod
-    def verify_user(cls,username, password):
-        """
-        method to verify whether the user is in our user_list or not
-        """
-        existing_user = ""
-        for user in User.user_list:
-            if(user.username == username and user.password == password):
-                    existing_user == user.username
-        return existing_user
-
 
     def save_credentials(self):
         """
